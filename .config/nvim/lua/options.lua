419912c58e3461ccad62254608dee654d3362181
nielwyn <nghoangnghiabt@gmail.com>
2025-12-19 14:04:48 +0700

    Revert tmux config

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,21 +1,22 @@
 local options = {
+  number         = false,
+  relativenumber = true,
+  signcolumn     = "yes",
+  colorcolumn    = "80",
   statuscolumn   = " %s%l%=%r",
   laststatus     = 4,
+  scrolloff      = 10,
+  sidescrolloff  = 10,
+  winborder      = "single",
+  hlsearch       = false,
   breakindent    = true,
+  smartindent    = true,
   tabstop        = 8,
   softtabstop    = 2,
   shiftwidth     = 2,
   expandtab      = true,
-  number         = false,
-  relativenumber = true,
-  signcolumn     = "yes",
   textwidth      = 80,
-  scrolloff      = 10,
-  sidescrolloff  = 10,
-  winborder      = "single",
   foldenable     = false,
-  colorcolumn    = "80",
-  smartindent    = true
 }
 
 for name, value in pairs(options) do
