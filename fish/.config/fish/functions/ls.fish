bca15c3110e61c2b5083211a498316255b28214d
nielwyn <nghoangnghiabt@gmail.com>
2026-03-14 08:32:18 +0000

    chore: handle ls file permission separately

--- fish/.config/fish/functions/ls.fish
+++ fish/.config/fish/functions/ls.fish
@@ -7,31 +7,26 @@ function ls --wraps=ls --description 'ls with highlighted file permissions'
 end
 
 function __highlight_permissions
+    set -l N (set_color normal)
     while read -l line
         if string match -qr '^[dlbcps\-][rwxsStT\-]{9}' -- $line
-            set -l perm (string sub -l 10 -- $line)
+            set -l t (string sub -l 1 -- $line)
+            set -l bits (string sub -s 2 -l 9 -- $line)
             set -l rest (string sub -s 11 -- $line)
-            set -l out ""
-            for i in (seq 1 10)
-                set -l c (string sub -s $i -l 1 -- $perm)
-                if test $i -eq 1
-                    switch $c
-                        case d;   set out $out(set_color --bold blue)$c(set_color normal)
-                        case l;   set out $out(set_color --bold cyan)$c(set_color normal)
-                        case '-'; set out $out(set_color brblack)$c(set_color normal)
-                        case '*'; set out $out(set_color --bold magenta)$c(set_color normal)
-                    end
-                else
-                    switch $c
-                        case r;         set out $out(set_color green)$c(set_color normal)
-                        case w;         set out $out(set_color red)$c(set_color normal)
-                        case x s S t T; set out $out(set_color --bold yellow)$c(set_color normal)
-                        case '-';       set out $out(set_color brblack)$c(set_color normal)
-                        case '*';       set out $out$c
-                    end
-                end
+
+            switch $t
+                case d;   set t (set_color --bold blue)$t$N
+                case l;   set t (set_color --bold cyan)$t$N
+                case '-'; set t (set_color brblack)$t$N
+                case '*'; set t (set_color --bold magenta)$t$N
             end
-            printf '%s\n' $out$rest
+
+            set bits (string replace -ar 'r' (set_color green)r$N $bits)
+            set bits (string replace -ar 'w' (set_color red)w$N $bits)
+            set bits (string replace -ar '([xsStT])' (set_color --bold yellow)'$1'$N $bits)
+            set bits (string replace -ar -- '-' (set_color brblack)-$N $bits)
+
+            printf '%s\n' $t$bits$rest
         else
             printf '%s\n' $line
         end
